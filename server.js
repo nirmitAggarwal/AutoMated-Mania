07ed67c11671fb96c74b4bc2d57e5e7576a0d2c0
nirmit152007 <nirmitjee@gmail.com>
2024-08-23 05:34:40 +0530

    n

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const axios = require("axios");
 
-const apiKey = "7043ac10-9ef2-43b1-94eb-758b384b7d71";
-const slackId = "U07D6EE31AB";
-const word = "PrJS"; // Change this to your desired word
+const apiKey = "d7b6cfbb-5942-4104-9c59-9fd54cddfe35";
+const slackId = "U07HYRFR8P3";
+const word = "project main "; // Change this to your desired word
 
-let sessionIndex = 1;
+let sessionIndex = 01;
 
 const startSession = async () => {
   const sessionName = `${word} ${sessionIndex}`;
@@ -21,14 +21,14 @@ const startSession = async () => {
     );
     if (response.data.ok) {
       console.log(`Session started: ${sessionName}`);
-      sessionIndex++;
-      setTimeout(startSession, 65 * 60 * 1000); // Start new session after 65 minutes
+      sessionIndex++;``
+      setTimeout(startSession, 60.2 * 60 * 1000); // Start new session after 65 minutes
     } else {
       throw new Error("Failed to start session");
     }
   } catch (error) {
     console.error(`Failed to start session: ${error.message}`);
-    setTimeout(startSession, 10 * 60 * 1000); // Retry after 10 minutes
+    setTimeout(startSession, 3 * 60 * 1000); // Retry after 3 minutes
   }
 };
 
